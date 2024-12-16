eb51115d188248220e222856cb2acd7f99b0592b
TommyExc <157232438+TommyExc@users.noreply.github.com>
2024-12-16 23:55:16 +0300

    Update BookingTest.java

--- src/main/java/booking2/BookingTest.java
+++ src/main/java/booking2/BookingTest.java
@@ -1,7 +1,6 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 import org.junit.Test;
-
 import static com.codeborne.selenide.Selenide.*;
 
 public class BookingTest {
@@ -11,25 +10,29 @@ public class BookingTest {
     public void testBooking() {
         BookingPage bookingPage = new BookingPage();
 
-
         bookingPage.openBookingPage();
 
-        String city = "Paris"; // Можно выбрать другой город
+        String city = "Дубай"; // Можно выбрать другой город
         bookingPage.searchForCity(city);
-
 
         bookingPage.selectRandomDates();
 
         bookingPage.clickSearchButton();
 
         bookingPage.clickMapButton();
-
         bookingPage.hoverOverFirstHotel();
+
         String hotelName = bookingPage.getHotelName();
         String hotelStars = bookingPage.getHotelStars();
         String hotelRating = bookingPage.getHotelRating();
         String hotelPrice = bookingPage.getHotelPrice();
 
+        System.out.println("Отель: " + hotelName);
+        System.out.println("Звезды: " + hotelStars);
+        System.out.println("Рейтинг: " + hotelRating);
+        System.out.println("Цена: " + hotelPrice);
 
         bookingPage.clickOnHotelMarker();
+        
     }
+}
